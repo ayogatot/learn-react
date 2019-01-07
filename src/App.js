27edc0c8a9a107b9051e27ef37c59886e3426f8e
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-07 13:34:49 +0700

    Create React App basic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo-github.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,16 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>My github profile</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/ayogatot"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            @ayogatot
           </a>
         </header>
       </div>
