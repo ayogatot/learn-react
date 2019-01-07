e78fc87bebb81feb8bc9ce7f39b7ccae91716835
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-07 19:17:40 +0700

    Make pokemon card

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import "./Pokemon.css";
+
+const topCollection = [1, 2, 3, 4, 5, 6 , 7 , 8];
 
 class Pokemon extends Component {
   render() {
     return (
-      <div>
-         <p>Pokemon</p>
+      <div className="card">
+        {topCollection.map((item, index) => {
+          return (
+            <span className="card-item">
+              <img key={index} src={`assets/${index + 1}.png`} alt="Media" />
+              <div className="">
+
+              </div>
+            </span>
+          );
+        })}
       </div>
     );
   }
