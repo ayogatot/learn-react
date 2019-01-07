7b0b481f8123709d04201240ab0f515c97dab571
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-07 22:19:50 +0700

    add new section

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import "./App.css";
 
 import Pokemon from "./Pokemon";
 import Navbar from "./Navbar";
+import Pokemon2 from "./Pokemon2";
 
 class App extends Component {
   render() {
-    return <div className="App">
+    return (
+      <div className="App">
         <Navbar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Pokémon Genereation 1</p>
         </header>
         <div className="container">
-        <h2>Pokémon Fire Red</h2>
+          <h2>Pokémon Top Collection</h2>
           <Pokemon />
         </div>
-      <footer className="footer-bottom">&copy;PokemonCompany</footer>
-      </div>;
+        <div className="container-middle">
+          <h2>Pokémon Mega Collection</h2>
+          <Pokemon2 />
+        </div>
+        <footer className="footer-bottom">&copy;PokemonCompany</footer>
+      </div>
+    );
   }
 }
 
